d4a2e8df6fdafe1e3f8e9be8cfc5e8c604e12ddc
lianyipython <37836285+lianyipython@users.noreply.github.com>
2019-05-07 23:33:30 +0800

    增加了read_from_nc

--- io/read_DataArray.py
+++ io/read_DataArray.py
@@ -133,3 +133,183 @@ def create_micaps4(micaps_abspath, grid_values, label,year,month,day,begin_hour,
     np.savetxt(micaps_abspath, grid_values, delimiter='  ',
                fmt='%4.6f', header=title,comments='')
     print('Create [%s] success'%micaps_abspath)
+
+def read_from_nc(filename,member = None,level = None,time = None,dt = None,lat = None,lon = None):
+    ds0 = xr.open_dataset(filename)
+    drop_list = []
+    ds = xr.Dataset()
+    #1判断要素成员member
+    if(member is None):
+        member = "member"
+    if member in ds0.coords or member in list(ds0):
+        if member in ds0.coords:
+            members = ds0.coords[member]
+        else:
+            members = ds0[member]
+            drop_list.append(member)
+
+        ds.coords["member"] = ("member", members)
+        attrs_name = list(members.attrs)
+        for key in attrs_name:
+            ds.member.attrs[key] = members.attrs[key]
+    else:
+        ds.coords["member"] = ("member", [0])
+
+    #2判断层次level
+    if (level is None):
+        if "level" in ds0.coords or "level" in list(ds0):
+            level = "level"
+        elif "lev" in ds0.coords or "lev" in list(ds0):
+            level = "lev"
+    if level in ds0.coords or level in list(ds0):
+        if level in ds0.coords:
+            levels = ds0.coords[level]
+        else:
+            levels = ds0[level]
+            drop_list.append(level)
+        ds.coords["level"] = ("level", levels)
+        attrs_name = list(levels.attrs)
+        for key in attrs_name:
+            ds.level.attrs[key] = levels.attrs[key]
+    else:
+        ds.coords["level"] = ("level", [0])
+
+    # 3判断时间time
+    if(time is None):
+        if "time" in ds0.coords or "time" in list(ds0):
+            time = "time"
+        elif "t" in ds0.coords:
+            time = "t"
+    if time in ds0.coords or time in list(ds0):
+        if time in ds0.coords:
+            times = ds0.coords[time]
+        else:
+            times = ds0[time]
+        ds.coords["time"] = ("time", times)
+        attrs_name = list(times.attrs)
+        for key in attrs_name:
+            ds.time.attrs[key] = times.attrs[key]
+    else:
+        ds.coords["time"] = ("time", [0])
+
+    # 4判断时效dt
+    if (dt is None):
+        dt = "dt"
+    if dt in ds0.coords or dt in list(ds0):
+        if dt in ds0.coords:
+            dtss = ds0.coords[dt]
+        else:
+            dts = ds0[dt]
+            drop_list.append(dt)
+
+        ds.coords["dt"] = ("dt", dts)
+        attrs_name = list(dts.attrs)
+        for key in attrs_name:
+            ds.dt.attrs[key] = dts.attrs[key]
+    else:
+        ds.coords["dt"] = ("dt", [0])
+
+    #5判断纬度lat
+    if(lat is None):
+        if "lat" in ds0.coords or "lat" in list(ds0):
+            lat = "lat"
+        elif "lat" in ds0.coords or "lat" in list(ds0):
+            lat = "lat"
+    if lat in ds0.coords or lat in list(ds0):
+        if lat in ds0.coords:
+            lats = ds0.coords[lat]
+        else:
+            lats = ds0[lat]
+            drop_list.append(lat)
+        dims = lats.dims
+        if len(dims) == 1:
+            ds.coords["lat"] = ("lat", lats)
+        else:
+            if "lon" in dims[0].lower() or "x" in dims.lower():
+                lats = lats.values.T
+            ds.coords["lat"] = (("lat","lon"), lats)
+        attrs_name = list(lats.attrs)
+        for key in attrs_name:
+            ds.lat.attrs[key] = lats.attrs[key]
+    else:
+        ds.coords["lat"] = ("lat",[0])
+
+    #6判断经度lon
+    if(lon is None):
+        if "lon" in ds0.coords or "lon" in list(ds0):
+            lon = "lon"
+        elif "lon" in ds0.coords or "lon" in list(ds0):
+            lon = "lon"
+    if lon in ds0.coords or lon in list(ds0):
+        if lon in ds0.coords:
+            lons = ds0.coords[lon]
+        else:
+            lons = ds0[lon]
+            drop_list.append(lon)
+
+        dims = lons.dims
+        if len(dims) == 1:
+            ds.coords["lon"] = ("lon", lons)
+        else:
+            if "lon" in dims[0].lower() or "x" in dims.lower():
+                lons = lons.values.T
+            ds.coords["lon"] = (("lat","lon"), lons)
+        attrs_name = list(lons.attrs)
+        for key in attrs_name:
+            ds.lon.attrs[key] = lons.attrs[key]
+    else:
+        ds.coords["lon"] = ("lon",[0])
+
+    name_list = list((ds0))
+    for name in name_list:
+        if name in drop_list: continue
+        da = ds0[name]
+        shape = da.values.shape
+        size = 1
+        for i in range(len(shape)):
+            size = size * shape[i]
+        if size > 1:
+            dims = da.dims
+            dim_order = {}
+
+            for dim in dims:
+                if  "member" in dim.lower():
+                    dim_order["member"] = dim
+                elif "time" in dim.lower():
+                    dim_order["time"] = dim
+                elif "dt" in dim.lower():
+                    dim_order["dt"] = dim
+                elif "lev" in dim.lower():
+                    dim_order["level"] = dim
+                elif "lat" in dim.lower() or 'y' in dim.lower():
+                    dim_order["lat"] = dim
+                elif "lon" in dim.lower() or 'x' in dim.lower():
+                    dim_order["lon"] = dim
+            if "member" not in dim_order.keys():
+                dim_order["member"] = "member"
+                da = da .expand_dims("member")
+            if "time" not in dim_order.keys():
+                dim_order["time"] = "time"
+                da = da .expand_dims("time")
+            if "level" not in dim_order.keys():
+                dim_order["level"] = "level"
+                da = da .expand_dims("level")
+            if "dt" not in dim_order.keys():
+                dim_order["dt"] = "dt"
+                da = da .expand_dims("dt")
+            if "lat" not in dim_order.keys():
+                dim_order["lat"] = "lat"
+                da = da .expand_dims("lat")
+            if "lon" not in dim_order.keys():
+                dim_order["lon"] = "lon"
+                da = da .expand_dims("lon")
+            da = da.transpose(dim_order["member"],dim_order["level"],dim_order["time"],
+                              dim_order["dt"],dim_order["lat"],dim_order["lon"])
+            ds[name] = (("member","level","time","dt","lat","lon"),da)
+            attrs_name = list(da.attrs)
+            for key in attrs_name:
+                ds[name].attrs[key] = da.attrs[key]
+    attrs_name = list(ds0.attrs)
+    for key in attrs_name:
+        ds.attrs[key] = ds0.attrs[key]
+    return ds
